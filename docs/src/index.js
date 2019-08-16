068e233f2117fa36e4671de37bbc93f266d0c0ac
duxiaofeng-github <297951292@qq.com>
2019-08-16 19:22:37 +0800

    update docs

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,36 +1,36 @@
 import JuicyPlayer from "../../dist";
 
 const src1080p = {
-  src: "http://7qnbk6.com1.z0.glb.clouddn.com/interstellar_1080p.mp4",
+  src: "http://7qnbk6.com1.z0.glb.clouddn.com/Interstellar_1080p.mp4",
   name: "1080p",
   mimetype: "video/mp4",
 };
 
 const srcHD = {
-  src: "http://7qnbk6.com1.z0.glb.clouddn.com/interstellar_HD.mp4",
+  src: "http://7qnbk6.com1.z0.glb.clouddn.com/Interstellar_HD.mp4",
   name: "超清",
   mimetype: "video/mp4",
 };
 // const srcSD = {
 //   src:
 //     "rtmp://ali-live.zaojiu.com/zaojiu/59313bd2647259000183041d_sd?auth_key=1512472740-0-0-845571a71200e6c83807d4e399f3ad00",
-//   // src: 'http://7qnbk6.com1.z0.glb.clouddn.com/interstellar_MD.mp4',
+//   // src: 'http://7qnbk6.com1.z0.glb.clouddn.com/Interstellar_MD.mp4',
 //   name: "高清",
 //   mimetype: "rtmp/mp4",
 // };
 const srcMD = {
-  src: "http://7qnbk6.com1.z0.glb.clouddn.com/interstellar_MD.mp4",
+  src: "http://7qnbk6.com1.z0.glb.clouddn.com/Interstellar_MD.mp4",
   name: "高清",
   mimetype: "video/mp4",
 };
 // const srcMD = {
 //   src: '//edge.flowplayer.org/flowplayer-700.flv',
-//   // src: 'http://7qnbk6.com1.z0.glb.clouddn.com/interstellar_MD.mp4',
+//   // src: 'http://7qnbk6.com1.z0.glb.clouddn.com/Interstellar_MD.mp4',
 //   name: '高清',
 //   mimetype: 'video/mp4'
 // };
 const srcSD = {
-  src: "http://7qnbk6.com1.z0.glb.clouddn.com/interstellar_SD.mp4",
+  src: "http://7qnbk6.com1.z0.glb.clouddn.com/Interstellar_SD.mp4",
   name: "标清",
   mimetype: "video/mp4",
 };
